330a628efe4460711c69dca670f2ba2479d697d4
pepeboricua <joseoronozjr@gmail.com>
2023-02-18 01:44:50 +0000

    Halfway

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,38 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  return (
+    <div className="page">
+      <div className="container justify-content-flex">
+        <div className="row">
+          <div className="col">
+            <div className="clock">
+              <i className="fa-solid fa-clock"></i>
+            </div>
+          </div>
+          <div className="col">
+            <div className="onehunthous">1</div>
+          </div>
+          <div className="col">
+            <div className="tenthous">1</div>
+          </div>
+          <div className="col">
+            <div className="thous">1</div>
+          </div>
+          <div className="col">
+            <div className="hunds">1</div>
+          </div>
+          <div className="col">
+            <div className="tens">1</div>
+          </div>
+          <div className="col">
+            <div className="ones">1</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
